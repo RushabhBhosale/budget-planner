9bad9f221f838757ebb23d3b8acd2b3ddc33fcc7
Rushabh Bhosale <bhosalerushabh0@gmail.com>
2024-09-27 15:28:52 +0530

    angular app created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'budget',
+    loadChildren: () =>
+      import('./budget-planner/budget-planner.module').then(
+        (m) => m.BudgetPlannerModule
+      ),
+  },
+];
